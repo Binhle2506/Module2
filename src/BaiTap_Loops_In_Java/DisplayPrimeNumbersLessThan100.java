30cd95666716b68115fd73f98eb6e584008bb25e
Binhle2506 <binhle00625@gmail.com>
2025-02-16 15:45:01 +0700

    baiTap

--- src/BaiTap_Loops_In_Java/DisplayPrimeNumbersLessThan100.java
+++ src/BaiTap_Loops_In_Java/DisplayPrimeNumbersLessThan100.java
@@ -1,5 +1,16 @@
 package BaiTap_Loops_In_Java;
 
 public class DisplayPrimeNumbersLessThan100 {
+    public static void main(String[] args) {
+        int count = 0 ;
+        int number = 2 ;
+        while ( count < 99 ){
+            if ( DisplayFirst20PrimeNumbers.isPrime(number) ){
+                System.out.print(number + " ");
+                count++;
+            }
+            number++;
+        }
 
+    }
 }
