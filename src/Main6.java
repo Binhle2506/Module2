4187df9c7b6603ad68fad5ba9f867cbdc632eb2a
Binhle2506 <binhle00625@gmail.com>
2025-02-24 20:59:51 +0700

    baiTap

---
+++ src/Main6.java
@@ -0,0 +1,7 @@
+public class Main6 {
+    public static void main(String[] args) {
+        Circle mot = new Circle(5);
+        System.out.println("Circle radius " + mot.getRadius());
+        System.out.println("Circle area " + mot.getArea());
+    }
+}
